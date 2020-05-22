418886682ace7600b0303fbc98fae9d6e99936de
qnedy <altmaisefequatro@gmail.com>
2020-05-22 02:15:06 -0300

    started logon page

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
+
+import './styles.css';
+
+import heroesImage from '../../assets/heroes.png';
+import logoImage from '../../assets/logo.svg';
+
+const LogonScreen = () => {
+  return(
+    <>
+      <div className="logon-container">
+        <section className="form">
+          <img src={logoImage} alt="Be The Hero" />
+
+          <form action="">
+            <h1>Faça seu logon</h1>
+
+            <input placeholder="Sua ID" />
+            <button type="submit">Entrar</button>
+
+            <a href="/register">
+              <FiLogIn size={16} color="#e02041"/>
+              Não tenho cadastro
+            </a>
+          </form>
+        </section>
+
+        <img src={heroesImage} alt="heroes" />
+      </div>
+    </>
+  );
+}
+
+export default LogonScreen;
