5a24a72841359ea585cb3f66baa17066c30501c1
qnedy <altmaisefequatro@gmail.com>
2020-05-23 16:42:02 -0300

    frontend finished

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LogonPage from './pages/Logon';
 import RegisterPage from './pages/Register';
 import ProfilePage from './pages/Profile';
+import NewIncidentPage from './pages/NewIncident';
 
 const Routes = () => {
   return(
@@ -12,6 +13,7 @@ const Routes = () => {
         <Route path="/" exact component={LogonPage} />
         <Route path="/register" component={RegisterPage} />
         <Route path="/profile" component={ProfilePage} />
+        <Route path="/incidents/new" component={NewIncidentPage} />
       </Switch>
     </BrowserRouter>
   );
